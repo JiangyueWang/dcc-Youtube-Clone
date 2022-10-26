d067730bdb705f7daa3869af0f228308744e6479
Jiang <jiangyue0380@gmail.com>
2022-10-27 00:32:57 +1100

    updated CommentSeralizer class in comments app

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -5,4 +5,5 @@ from backend.comments.models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['video_id', 'text', 'like', 'dislike', 'user_id']
+        fields = ['video_id', 'text', 'like', 'dislike', 'user']
+        depth = 1
