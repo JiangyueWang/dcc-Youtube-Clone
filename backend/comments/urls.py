dc808b27ded90156ba1002329112e83865124a70
Jiang <jiangyue0380@gmail.com>
2022-11-01 11:47:25 +1100

    updated urls.py of comments app + modified .gitignore

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.post_comment),
     path('<str:videoId>/', views.view_comments),
     path('like/<int:comment_id>/', views.comment_like),
-    path('dislike/<int:comment_id>/', views.comment_like),
+    path('dislike/<int:comment_id>/', views.comment_dislike),
     path('reply/<int:comment_id>/', views.reply_comment),
     path('replies/<int:comment_id>/', views.view_comment_replies),
 ]
